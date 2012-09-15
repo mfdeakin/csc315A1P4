6e69dd0e1d2e5a7e9c9f1932ee9f9d4b266404e9
mfdeakin <mdever44@gmail.com>
2012-09-15 19:55:36 -0400

    Mostly finished, just a problem with the function intersections

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #include <GL/gl.h>
 #include <GL/glut.h>
@@ -12,13 +13,15 @@
 #define VIEWWIDTH 500.0
 #define OFFHEIGHT 100
 #define OFFWIDTH 100
-#define CENTERX 350
-#define CENTERY 350
+
+#define MINX -30.0f
+#define MAXX 30.0f
+#define MINY -500.0f
+#define MAXY 500.0f
 
 struct pt {
 	GLint x;
 	GLint y;
-	bool valid;
 } linestart, lineend, wnddim, circle[800];
 
 bool ptCompare(struct pt lhs, struct pt rhs)
@@ -30,9 +33,7 @@ bool ptCompare(struct pt lhs, struct pt rhs)
 
 struct pt dispToCoord(struct pt pos);
 void drawView(void);
-void drawCircle(float radius);
-void drawCircle2(float radius);
-void drawLine(void);
+void drawFunc(void);
 void display(void);
 void resize(GLsizei width, GLsizei height);
 void mpress(int btn, int state, int x, int y);
@@ -41,150 +42,44 @@ void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	drawView();
-	drawLine();
-	/* drawCircle(200); */
+	drawFunc();
 	glFlush();
  	glutSwapBuffers();
-}
-
-void drawLine(void)
-{
-	struct pt bound_bl = dispToCoord((struct pt){0, 0, false}),
-		bound_tr = dispToCoord((struct pt){VIEWWIDTH, VIEWHEIGHT, false});
-	if(!linestart.valid || !lineend.valid ||
-		 ptCompare(linestart, lineend) ||
-		 linestart.x < bound_bl.x ||
-		 linestart.x > bound_tr.x ||
-		 lineend.x < bound_bl.x ||
-		 lineend.x > bound_tr.x ||
-		 linestart.y < bound_bl.y ||
-		 linestart.y > bound_tr.y ||
-		 lineend.y < bound_bl.y ||
-		 lineend.y > bound_tr.y)
-		return;
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glBegin(GL_POINTS);
-	GLint deltax = lineend.x - linestart.x,
-		deltay = lineend.y - linestart.y,
-		x = 0,
-		y = 0;
-	
-	GLfloat m = (float)deltay / deltax;
-
-	if(m == INFINITY) {
-		while(y < deltay) {
-			glVertex2i(linestart.x, linestart.y + y);
-			y++;
-		}
-	}
-	else if(m == -INFINITY) {
-		while(y > deltay) {
-			glVertex2i(linestart.x, linestart.y + y);
-			y--;
-		}
-	}
-	else {
-		float residual = 0;
-		while(x > deltax) {
-			x--;
-			int cnt = 0;
-			do {
-				glVertex2i(linestart.x + x, linestart.y + y);
-				cnt++;
-				y--;
-			} while(cnt < m + residual);
-			do {
-				glVertex2i(linestart.x + x, linestart.y + y);
-				cnt--;
-				y++;
-			} while(cnt > m + residual);
-			residual = m + residual - cnt;
-		}
-		while(x < deltax) {
-			x++;
-			int cnt = 0;
-			do {
-				glVertex2i(linestart.x + x, linestart.y + y);
-				cnt++;
-				y++;
-			} while(cnt < m + residual);
-			do {
-				glVertex2i(linestart.x + x, linestart.y + y);
-				cnt--;
-				y--;
-			} while(cnt > m + residual);
-			residual = m + residual - cnt;
-		}
-	}
-	glEnd();
-}
-
-float inCircle(float x, float y, float rad)
-{
-	return x * x + y * y - rad * rad;
-}
-
-void drawCircle(float radius)
-{
-	float curx = 0, cury = radius;
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glBegin(GL_POINTS);
-	while(cury >= curx) {
-		glVertex2i(CENTERX + curx, CENTERY + cury);
-		glVertex2i(CENTERX - curx, CENTERY + cury);
-		glVertex2i(CENTERX + curx, CENTERY - cury);
-		glVertex2i(CENTERX - curx, CENTERY - cury);
-
-		glVertex2i(CENTERX + cury, CENTERY + curx);
-		glVertex2i(CENTERX - cury, CENTERY + curx);
-		glVertex2i(CENTERX + cury, CENTERY - curx);
-		glVertex2i(CENTERX - cury, CENTERY - curx);
-		float old = inCircle(curx++, cury - 0.5, radius);
-		if(old >= 1) {
-			cury--;
-		}
-		else if(old >= 0) {
-			cury -= 0.5;
-		}
-	}
-	glEnd();
 }
 
 struct pt dispToCoord(struct pt pos)
 {
 	return (struct pt){
 		(pos.x + OFFWIDTH) * wnddim.x / (VIEWWIDTH + 2 * OFFWIDTH),
-			(pos.y + OFFHEIGHT) * wnddim.y / (VIEWHEIGHT + 2 * OFFHEIGHT),
-			true
+			(pos.y + OFFHEIGHT) * wnddim.y / (VIEWHEIGHT + 2 * OFFHEIGHT)
 			};
+}
+
+float dispToFuncX(int x)
+{
+	return (x - OFFWIDTH) * (MAXX - MINX) /
+		(VIEWWIDTH - 1) + MINX;
+}
+
+float dispToFuncY(int y)
+{
+	return (y - OFFHEIGHT) * (MAXY - MINY) /
+		(VIEWHEIGHT - 1) + MINY;
 }
 
 void mpress(int btn, int state, int x, int y)
 {
 	y = wnddim.y - y;
-	struct pt check;
-	check.x = 0;
-	check.y = 0;
-	check.valid = true;
-	struct pt checkbl = dispToCoord(check);
-	check.x = VIEWWIDTH;
-	check.y = VIEWHEIGHT;
-	check.valid = true;
-	struct pt checktr = dispToCoord(check);
-	if(x < checkbl.x || y < checkbl.y ||
-		 x > checktr.x || y > checktr.y)
-		return;
 	if(btn == GLUT_LEFT_BUTTON) {
-		linestart.x = x;
-		linestart.y = y;
-		linestart.valid = true;
-		glutPostRedisplay();
-	}
-	else if(btn == GLUT_RIGHT_BUTTON) {
-		lineend.x = x;
-		lineend.y = y;
-		lineend.valid = true;
-		glutPostRedisplay();
+		if(x < OFFWIDTH || x > OFFWIDTH + VIEWWIDTH ||
+			 y < OFFHEIGHT || y > OFFHEIGHT + VIEWHEIGHT) {
+			/* Outside the viewport */
+			printf("x: %d\ny: %d\n", x, y);
+		}
+		else {
+			printf("x: %f\ny: %f\n", dispToFuncX(x),
+						 dispToFuncY(y));
+		}
 	}
 }
 
@@ -192,9 +87,9 @@ void drawView(void)
 {
 	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_POINTS);
-	struct pt bound_bl = dispToCoord((struct pt){0, 0, false}),
-		bound_tr = dispToCoord((struct pt){VIEWWIDTH, VIEWHEIGHT, false}),
-		pos = {0, 0, true};
+	struct pt bound_bl = dispToCoord((struct pt){0, 0}),
+		bound_tr = dispToCoord((struct pt){VIEWWIDTH, VIEWHEIGHT}),
+		pos = {0, 0};
 	for(pos.x = bound_bl.x; pos.x < bound_tr.x; pos.x++)
 		for(pos.y = bound_bl.y; pos.y < bound_tr.y; pos.y++) {
 			glVertex2i(pos.x, pos.y);
@@ -202,11 +97,77 @@ void drawView(void)
 	glEnd();
 }
 
+float f(float x)
+{
+	return 500 * exp(-x * x / 150) * sin(2 * x);
+}
+
+float g(float x)
+{
+	return 300 * exp(-x * x / 300);
+}
+
+void drawFunc(void)
+{
+	int i;
+	glBegin(GL_POINTS);
+	float fY, gY;
+	for(i = OFFWIDTH; i < OFFWIDTH + VIEWWIDTH; i++) {
+		/* Calculate the functions X coordinate */
+		float funcX = dispToFuncX(i);
+		int j;
+		/* Cheating.
+		 * I considered using my line drawing algorithm,
+		 * and calculating the points of intersection of
+		 * the lines, which is probably the "good" way of
+		 * doing this, but didn't like that it would miss
+		 * points in functions. 
+		 * Now I still miss points, but considerably less,
+		 * at the expense of more computation. It also can
+		 * overwrite points which marked intersections,
+		 * which is also bad.
+		 * Obviously the best solution depends on the goal
+		 * of the program, but I don't think this is ever it,
+		 * especially since I don't really take the function
+		 * into account on how many points I calculate */
+		int vfY = INT_MAX,
+			vgY = INT_MAX,
+			vfYprev, vgYprev;
+		/* Number of points to draw per x coordinate */
+		const int xpCount = 64;
+		for(j = 0; j < 64; j++) {
+			fY = f(funcX);
+			gY = g(funcX);
+			funcX += 1 / (float)xpCount * (MAXX - MINX) / (VIEWWIDTH - 1);
+			/* So we take the functions into account at least
+			 * a little, don't draw points already drawn by
+			 * the function */
+			vfYprev = vfY;
+			vgYprev = vgY;
+			/* Convert the Y coordinate to the viewports coordinates */
+			vfY = (fY - MINY) * VIEWHEIGHT / (MAXY - MINY) + OFFWIDTH;
+			vgY = (gY - MINY) * VIEWHEIGHT / (MAXY - MINY) + OFFWIDTH;
+			if(vfY == vfYprev && vgY == vgYprev)
+				continue;
+			if(vfY == vgY) {
+				glColor3f(1.0f, 0.0f, 0.0f);
+				glVertex2i(i, vfY);
+			}
+			else {
+				glColor3f(0.0f, 0.0f, 1.0f);
+				glVertex2i(i, vfY);
+				glVertex2i(i, vgY);
+			}
+		}
+	}
+	puts("");
+	glEnd();
+}
+
 void resize(GLsizei width, GLsizei height)
 {
 	wnddim.x = width;
 	wnddim.y = height;
-	wnddim.valid = true;
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -222,13 +183,6 @@ void keypress(unsigned char key, int x, int y)
 	case 'Q':
 		exit(0);
 	}
-}
-
-int gcf(int a, int b)
-{
-	if(a % b)
-		return gcf(b, a % b);
-	return b;
 }
 
 int main(int argc, char **argv)
